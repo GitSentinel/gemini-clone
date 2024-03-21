cf3801c6f91d33dbf69d7317b24600780d15ee17
Jacinth Mahanta <mahanta.jacinth2021@gmail.com>
2024-03-21 16:52:42 +0530

    [ADD]: Sidebar UI completed! [UPD]: Folder struture updated!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Sidebar/>
+    </>
   )
 }
 
